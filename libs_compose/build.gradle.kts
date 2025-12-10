7e64402944651889c1dd339060e728f19ad8b152
Nguyễn Thành Nam <thanhnamzzz@users.noreply.github.com>
2025-12-10 22:04:02 +0700

    update build gradle

--- libs_compose/build.gradle.kts
+++ libs_compose/build.gradle.kts
@@ -1,83 +1,73 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	alias(libs.plugins.android.library)
-	alias(libs.plugins.kotlin.android)
-	alias(libs.plugins.kotlin.compose)
-	`maven-publish`
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
 }
 
 android {
-	namespace = "common.libs.compose"
-	compileSdk = 36
+    namespace = "common.libs.compose"
+    compileSdk = 36
 
-	publishing {
-		singleVariant("release") {
-			withSourcesJar()
-		}
-	}
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 
-	defaultConfig {
-		minSdk = 23
+    defaultConfig {
+        minSdk = 23
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
 
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-		}
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_17
-		targetCompatibility = JavaVersion.VERSION_17
-	}
-	kotlin {
-		compilerOptions {
-			jvmTarget.set(JvmTarget.JVM_17)
-		}
-	}
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
 
-	buildFeatures { compose = true }
+    buildFeatures { compose = true }
 }
 
 dependencies {
-	debugImplementation(libs.androidx.compose.ui.tooling)
-	debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.runtime)
-	implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.runtime)
+    implementation(libs.androidx.compose.ui)
 //	implementation(libs.androidx.compose.ui.graphics)
-	implementation(libs.androidx.compose.material3)
-	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui.tooling.preview)
 //	implementation(libs.androidx.compose.foundation)
 //	implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
 }
-//publishing {
-//	publications {
-//		register<MavenPublication>("release") {
-//			groupId = "common-libs"
-//			artifactId = "compose"
-//			version = "1.0"
-//			from(components["release"])
-//		}
-//	}
-//}
 
 publishing.publications {
-	create<MavenPublication>("release") {
-		afterEvaluate {
-			from(components["release"])
-		}
-		groupId = "common-libs"
-		artifactId = "compose"
-		version = "1.0"
-	}
+    create<MavenPublication>("release") {
+        afterEvaluate {
+            from(components["release"])
+        }
+        groupId = "common-libs"
+        artifactId = "compose"
+        version = "1.0"
+    }
 }
